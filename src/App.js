46951c6f54dbc7644ae4b83c391eb511065a0112
ymcla <ymclapp@yahoo.com>
2022-01-28 15:12:36 -0600

    added home, footer, header, navmenu, the css for all and updated app.js.  They are all working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Switch, Route } from 'react-router-dom';
+
+
+//Navigation
+import NavMenu from './components/partials/NavMenu';
+import Header from './components/partials/Header';
+import Footer from './components/partials/Footer';
+
+//public routes
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<NavMenu />
+<Header />
+
+<Switch>
+
+<Route path={['/', '/home']}>
+  <Home />
+</Route>
+
+</Switch>
+<Footer />
+
+</>
   );
 }
 
