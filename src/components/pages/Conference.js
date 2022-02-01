423c3ccfb6eb07cb0fb4e2038babb8cfd709d375
ymcla <ymclapp@yahoo.com>
2022-02-01 15:35:22 -0600

    added height vw, width 100%, and object-fit cover and the pictures are perfect

--- src/components/pages/Conference.js
+++ src/components/pages/Conference.js
@@ -27,17 +27,17 @@ export default function Home() {
             </div>
             <div className='conf-summary text-justify'>
                 <p>
-                The ATS International Conference is the home of pulmonary, critical care, and sleep professionals, from those in the earliest stages of their careers to those whose research or strides in clinical care has gained them international recognition. Each year, nearly 14,000 of these professionals choose to attend, present, and learn about the latest advances, meet with colleagues from around the world, and strike new collaborations. It is truly where today’s science meets tomorrow’s care.
-                <br /><br />
-                The International Conference is also multidisciplinary. Clinicians and researchers in many other fields attend the conference, thereby enriching conversations that lead to insights that ultimately improve patient care.
-                <br /><br />
-                Equally important, the ATS International Conference gathers the entire health care team in one place to share ideas across these medical and scientific disciplines. It is the ideal place for registered nurses, advanced practice nurses, respiratory therapists, and hospitalists to discuss their experiences, challenges, and research so that all can learn about the latest advances in understanding pulmonary disease, critical illness, and sleep disorders.
-                <br /><br />
-                And of course, the International Conference is truly international:
-                40 percent of attendees arrive from laboratories and patient care centers beyond the United States.
+                    The ATS International Conference is the home of pulmonary, critical care, and sleep professionals, from those in the earliest stages of their careers to those whose research or strides in clinical care has gained them international recognition. Each year, nearly 14,000 of these professionals choose to attend, present, and learn about the latest advances, meet with colleagues from around the world, and strike new collaborations. It is truly where today’s science meets tomorrow’s care.
+                    <br /><br />
+                    The International Conference is also multidisciplinary. Clinicians and researchers in many other fields attend the conference, thereby enriching conversations that lead to insights that ultimately improve patient care.
+                    <br /><br />
+                    Equally important, the ATS International Conference gathers the entire health care team in one place to share ideas across these medical and scientific disciplines. It is the ideal place for registered nurses, advanced practice nurses, respiratory therapists, and hospitalists to discuss their experiences, challenges, and research so that all can learn about the latest advances in understanding pulmonary disease, critical illness, and sleep disorders.
+                    <br /><br />
+                    And of course, the International Conference is truly international:
+                    40 percent of attendees arrive from laboratories and patient care centers beyond the United States.
                 </p>
                 <h3 className='conf-takeaway'>
-                Join your colleagues, and be the first to know.
+                    Join your colleagues, and be the first to know.
                 </h3>
             </div>
 
@@ -51,9 +51,11 @@ export default function Home() {
                         <Card.Img className='featured' variant='top' src={pic1} />
                         <Card.Body>
                             <Card.Text>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                            <br /><br/>
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                Your registration includes a networking reception and dinner with keynote address on Friday, May 13th as well as a full day of programming, continental breakfast, luncheon, plenty of networking time and a closing reception on Saturday, May 14th.
+                                <br /><br />
+                                Registration will open on Thursday, January 6, 2022 along with registration for the ATS 2022 International Conference. Attendance at the Respiratory Innovation Summit does not include OR require full registration for ATS 2022, but attendees are encouraged to be a part of our community.
+                                <br /><br />
+                                More information about ATS 2022 can be found here: https://conference.thoracic.org
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer>
@@ -85,9 +87,9 @@ export default function Home() {
                         <Card.Img className='featured' variant='top' src={pic3} />
                         <Card.Body>
                             <Card.Text>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                            <br /><br/>
-                            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                <br /><br />
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                             </Card.Text>
                         </Card.Body>
                         <Card.Footer>
