76cb74d6239263dc95c9ee86e32c9c12686f68ba
ymcla <ymclapp@yahoo.com>
2022-03-02 10:54:45 -0600

    customized Hit to highlight matching title during search

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,13 +1,27 @@
 //Algolia Search
-import '@algolia/autocomplete-theme-classic';
+// import '@algolia/autocomplete-theme-classic';
 import algoliasearch from 'algoliasearch/lite';
 
-import { InstantSearch, SearchBox, Hits, Index } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox, Hits, Index, Highlight } from 'react-instantsearch-dom';
+import { Card } from 'react-bootstrap';
+
+import '../../style.css';  //you can CSS the results here
 
 
 const searchClient = algoliasearch(
     "74W68YLA0N",
     "6849b26b52cd542e3f4e44e985634e9f"
+);
+const Hit = ({ hit }) => (
+    <p>
+        <Highlight attribute='attributes.title' hit={hit} tagName='mark' />
+    </p>
+);
+
+const Hit2 = ({ hit }) => (
+    <p>
+        <Highlight attribute = 'title' hit={hit} tagName='mark' />
+    </p>
 );
 
 
@@ -21,12 +35,12 @@ export default function Search() {
                 >
                 <SearchBox />
                 <p></p>
-                <p><strong>dev_ATSStore Results</strong></p>
-                <Hits />
+                <p className='results__location'>Algolia Index:  dev_ATSStore Results</p>
+                <Hits hitComponent={Hit} />
                 <p></p>
                 <Index indexName="dev_Manuals">
-                    <p><strong>dev_Manuals Results</strong></p>
-                    <Hits />
+                    <p className='results__location'>Algolia Index:  dev_Manuals Results</p>
+                    <Hits  hitComponent={Hit2} />
                 </Index>
             </InstantSearch>
         </div>
