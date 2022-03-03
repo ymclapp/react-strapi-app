cf3147c6c1496918f766419e70b18ec3110cf255
ymcla <ymclapp@yahoo.com>
2022-03-03 16:36:20 -0600

    added a note to the header file, added the assemblies

---
+++ src/components/pages/Assemblies.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCardHeader, MDBCardFooter, MDBBtn } from 'mdb-react-ui-kit';
+
+const assembAPI = 'http://localhost:1337/api/assemblies';
+
+
+
+export default function Assemblies() {
+
+    const [assemblies, setAssemblies] = useState({ data: [] });
+
+    useEffect(() => {
+        getAssembliesWithFetch();
+    }, []);
+
+    const getAssembliesWithFetch = async () => {
+        const response = await fetch(assembAPI);
+        const jsonData = await response.json({});
+        setAssemblies(jsonData);
+        console.log(jsonData);
+    };
+
+    return (
+        <>
+            <header className='assemblies-header'>
+                <h1 className='assemblies-title text-center'>Assemblies</h1>
+                <p className='assembly-definition justify-content'>
+                    Assemblies are subdivisions of the Society composed of members and affiliates with like interests within the broad fields of respiratory disease, and critical care medicine. The purpose of an Assembly is to improve the collection, interpretation, and dissemination of information, improve communication among the members, and participate in the planning of the International Conference. Assemblies are also responsible for creating and updating ATS Statements and Position Papers. Each ATS member may choose to join one Primary Assembly and up to two Secondary Assemblies.
+                </p>
+            </header>
+            <Container fluid>
+                {assemblies.data.map((assembly) => (
+                    <MDBCard style={{ maxWidth: '60em', padding: '2em', margin: '2em' }}>
+
+                        <MDBCardHeader key={assembly.id}>
+                            <a 
+                            href={assembly.attributes.assemblyPageLink} 
+                            rel="noopener noreferrer" 
+                            target="_blank"
+                            style={{textDecoration: 'none'}}
+                            >
+                                <strong>{assembly.attributes.assemblyName}</strong>
+                                </a>
+
+                        </MDBCardHeader>
+
+                        <MDBCardBody>
+                            {/* <MDBCardTitle>Special title treatment</MDBCardTitle> */}
+                            <MDBCardText alignment='justify'>{assembly.attributes.assemblyDescription}
+                            </MDBCardText>
+                            <MDBCardText>
+                                <a
+                                    href={assembly.attributes.assemblyBrochure}
+                                    rel="noopener noreferrer"
+                                    target="_blank"
+                                    style={{textDecoration: 'none'}}
+                                >
+                                    Assembly Brochure
+                                </a>
+                            </MDBCardText>
+                        </MDBCardBody>
+
+                        <MDBCardFooter className='assembly-chair text-muted'>
+                            Chair:  {assembly.attributes.assemblyChairName}
+                            ({assembly.attributes.assemblyChairEmail})
+                        </MDBCardFooter>
+
+                    </MDBCard>
+                ))}
+            </Container>
+        </>
+    );
+};
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// import { MDBTypography } from 'mdb-react-ui-kit';
+import { Alert } from 'react-bootstrap';
 
 import './Header.css';
 
@@ -7,6 +9,19 @@ const user = JSON.parse(localStorage.getItem('user'));
 export default function Header() {
   return (
     <>
+      <Alert variant='primary' className='note'>
+        <Alert.Heading><strong>Note:</strong></Alert.Heading>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum doloremque
+          officia laboriosam. Itaque ex obcaecati architecto! Qui necessitatibus delectus placeat illo rem id nisi
+          consequatur esse, sint perspiciatis soluta porro?
+        </p>
+        <hr />
+        <p className='mb-0'>
+          Whenever you need to, be sure to use margin utilities to keep things nice
+          and tidy.
+        </p>
+      </Alert>
       <header>
         <div
           className='p-5 text-center bg-img'
