df81fcc05ccf99b4d0f8a45f41d937e1923eb878
ymcla <ymclapp@yahoo.com>
2022-03-03 16:40:56 -0600

    added assemblies to navmenu and added a margin to the assemblies intro

--- src/components/pages/Assemblies.js
+++ src/components/pages/Assemblies.js
@@ -25,7 +25,7 @@ export default function Assemblies() {
         <>
             <header className='assemblies-header'>
                 <h1 className='assemblies-title text-center'>Assemblies</h1>
-                <p className='assembly-definition justify-content'>
+                <p className='assembly-definition justify-content' style={{margin: '2em'}}>
                     Assemblies are subdivisions of the Society composed of members and affiliates with like interests within the broad fields of respiratory disease, and critical care medicine. The purpose of an Assembly is to improve the collection, interpretation, and dissemination of information, improve communication among the members, and participate in the planning of the International Conference. Assemblies are also responsible for creating and updating ATS Statements and Position Papers. Each ATS member may choose to join one Primary Assembly and up to two Secondary Assemblies.
                 </p>
             </header>
@@ -62,7 +62,7 @@ export default function Assemblies() {
                         </MDBCardBody>
 
                         <MDBCardFooter className='assembly-chair text-muted'>
-                            Chair:  {assembly.attributes.assemblyChairName}
+                            Chair:  {assembly.attributes.assemblyChairName} {''}
                             ({assembly.attributes.assemblyChairEmail})
                         </MDBCardFooter>
 
